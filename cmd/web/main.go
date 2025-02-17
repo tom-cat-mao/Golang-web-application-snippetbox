ad7f28b26ecc0537a7853db27cbb06ac05a93110
Tom_Cat <maoy896@gmail.com>
2025-02-17 11:17:35 +0800

    add custom error log

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -116,8 +116,9 @@ func main() {
 	}
 
 	srv := &http.Server{
-		Addr:    *addr,
-		Handler: app.routes(),
+		Addr:     *addr,
+		Handler:  app.routes(),
+		ErrorLog: slog.NewLogLogger(logger.Handler(), slog.LevelError),
 	}
 
 	// Log a message indicating that the server is starting
