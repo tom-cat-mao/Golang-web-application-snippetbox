7de98654e329abbf18fc88df4b4bd3b4889f4dc2
Tom Cat <maoy896@gmail.com>
2025-03-29 23:41:10 +0800

    Add end-to-end tests for the snippet creation route, verifying redirection for unauthenticated users and form display for authenticated users.

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -240,3 +240,36 @@ func TestUserSignup(t *testing.T) {
 		})
 	}
 }
+
+// An end-to-end test for the GET /snippet/create route.
+// - Unauthenticated users are redirected to the login form.
+// - Authenticated users are shown the form to create a new snippet.
+func TestSnippetCreate(t *testing.T) {
+	app := newTestApplication(t)
+
+	ts := newTestServer(t, app.routes())
+	defer ts.server.Close()
+
+	t.Run("Unauthenticated", func(t *testing.T) {
+		code, headers, _ := ts.get(t, "/snippet/create")
+
+		assert.Equal(t, code, http.StatusSeeOther)
+		assert.Equal(t, headers.Get("Location"), "/user/login")
+	})
+
+	t.Run("authenticated", func(t *testing.T) {
+		_, _, body := ts.get(t, "/user/login")
+		csrfToken := extractCSRFToken(t, body)
+
+		form := url.Values{}
+		form.Add("email", "alice@example.com")
+		form.Add("password", "pa$$word")
+		form.Add("csrf_token", csrfToken)
+		ts.postForm(t, "/user/login", form)
+
+		code, _, body := ts.get(t, "/snippet/create")
+
+		assert.Equal(t, code, http.StatusOK)
+		assert.StringContains(t, body, "<form action=\"/snippet/create\" method=\"POST\">")
+	})
+}
